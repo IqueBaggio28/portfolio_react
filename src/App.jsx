447969ec59ee3bde3c66128a216628b6f5d926d8
IqueBaggio28 <iquebaggio@gmail.com>
2025-04-24 15:41:45 -0400

    saving for a test

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ import Button from '@mui/material/Button';
 
 
 function App() {
+    const [theme, setTheme] = useState("light");
+
 
   return (
-   <div className="flex gap-20">
-       <Button variant="contained" className="bg-pink-700">
-           Hello World
-       </Button>
-       <p>This is a test</p>
-
-   </div>
+      <div className="bg-gray-100 w-100 h-100 rounded-lg flex flex-col items-start justify-around p-6 dark:bg-gray-900">
+            <h1 className="text-black p-4 dark:text-white">Title</h1>
+          <p className="text-black text-lg pr-3 dark:text-white">This is my normal text, tailwind is kinda confusing but its cool.</p>
+          <button className="bg-blue-100 text-blue-900 py-5 px-10 :hoverborder-2 dark:bg-blue-900 dark:text-blue-100" onClick={() => setTheme(prev => !prev)} >MODE</button>
+      </div>
   );
 }
 
