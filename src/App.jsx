cba18ecd00b55ca9ba548cfddf6455699d6d9e2c
IqueBaggio28 <iquebaggio@gmail.com>
2025-04-24 16:09:58 -0400

    added shadcn/ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import Button from '@mui/material/Button';
-
+import { Button } from "@/components/ui/button"
 
 function App() {
     const [theme, setTheme] = useState("light");
@@ -12,6 +11,7 @@ function App() {
             <h1 className="text-black p-4 dark:text-white">Title</h1>
           <p className="text-black text-lg pr-3 dark:text-white">This is my normal text, tailwind is kinda confusing but its cool.</p>
           <button className="bg-blue-100 text-blue-900 py-5 px-10 :hoverborder-2 dark:bg-blue-900 dark:text-blue-100" onClick={() => setTheme(prev => !prev)} >MODE</button>
+          <Button>shadcn Button</Button>
       </div>
   );
 }
