0c98ff2440602f111bae35fcb8ec6f1565e8bd15
IqueBaggio28 <iquebaggio@gmail.com>
2025-05-14 09:47:52 -0400

    finished mobile view for experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -13,23 +13,53 @@ const Experience = ({sectionHeader}) => {
     const [expandedIndex, setExpandedIndex] = useState(null);
 
     const companies = [
-        {name: 'Rentvine', title: 'FRONT-END DEVELOPER (I)', companyImg: 'images/rv-icon.png', description: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ultricies tincidunt venenatis. Suspendisse potenti. In justo dolor, ultrices ut vulputate id, dapibus eget nunc. Donec tempus est ante', 'Phasellus sit amet neque non ligula finibus ullamcorper. Proin facilisis fermentum dui, a efficitur erat. Sed accumsan rhoncus erat, dapibus malesuada mauris vulputate in.' , 'Aenean consequat condimentum nisi, eget iaculis libero sodales lacinia. Donec nec orci vitae mauris iaculis condimentum. Ut eu erat nisi. Cras ipsum velit, accumsan a scelerisque vitae, vehicula in nulla. Etiam vel dictum dolor'], date: 'Nov 2024 - Present', stacks: [{icon: faJsSquare, color: 'yellow'}, {icon: faHtml5, color: '#e34c26'}, {icon: faCss, color: '#264de4'}, {icon: faPhp, color: '#8892be' }]},
-        {name: 'Ottimizza Ltda.', title: 'SFTW ENGINEERING INTERN ', companyImg: 'images/ottimizza-icon.png', description: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ultricies tincidunt venenatis. Suspendisse potenti. In justo dolor, ultrices ut vulputate id, dapibus eget nunc. Donec tempus est ante', 'Phasellus sit amet neque non ligula finibus ullamcorper. Proin facilisis fermentum dui, a efficitur erat. Sed accumsan rhoncus erat, dapibus malesuada mauris vulputate in.' , 'Aenean consequat condimentum nisi, eget iaculis libero sodales lacinia. Donec nec orci vitae mauris iaculis condimentum. Ut eu erat nisi. Cras ipsum velit, accumsan a scelerisque vitae, vehicula in nulla. Etiam vel dictum dolor'], date: 'June 2023 - Aug 2023', stacks: [{icon: faJava, color: '#f89820'}, {icon: faDatabase, color: '#3776ab'}, {icon: faAws, color: '#ff9900'}]},
-        {name: 'Dendritic Hub', title: 'RESEARCH ASSISTANT', companyImg: 'images/fgcu-icon.png', description: 'blablablablablabla', date: 'Nov 2023 - Nov 2024', stacks: [{icon: faPython, color: '#3776ab'}]},
-        {name: 'U.A. Whitaker College of Engineering', title: 'SFTW ENGINEERING TUTOR', companyImg: 'images/fgcu-icon.png', description: 'blablablablablabla', date: 'Aug 2023 - Nov 2024', stacks: [{icon: faPython, color: '#3776ab'}, {icon: faJava, color: '#f89820'}, {svg: 'images/cpp-icon.svg' , color: '#3776ab'}, {svg: 'images/c-icon.svg' , color: '#3776ab'}]},
-        {name: 'U.A. Whitaker College of Engineering', title: 'TEACHING ASSISTANT ', companyImg: 'images/fgcu-icon.png', description: 'blablablablablabla', date: 'Aug 2023 - Nov 2024', stacks: [{icon: faPython, color: '#3776ab'}, {icon: faJava, color: '#f89820'}, {svg: 'images/cpp-icon.svg' , color: '#3776ab'}, {svg: 'images/c-icon.svg' , color: '#3776ab'}]},
+        {name: 'Rentvine', title: 'FRONT-END DEVELOPER (I)', companyImg: 'images/rv-icon.png', description: [
+                'At Rentvine, I work with a full-stack team to build and maintain dynamic web applications. My primary role focuses on implementing front-end features using React.',
+                'I enhance user interfaces for performance and accessibility, ensuring a smooth experience across devices. I also work with PHP and SQL to manage backend logic and maintain database reliability.',
+                'I contribute to debugging tasks, write scalable code, and optimize workflows. This improves development efficiency and minimizes user friction.',
+                'Regular code reviews help me maintain high-quality standards and stay current with modern web development practices.'
+            ]
+            , date: 'Nov 2024 - Present', stacks: [{icon: faJsSquare, color: 'yellow'}, {icon: faHtml5, color: '#e34c26'}, {icon: faCss, color: '#264de4'}, {icon: faPhp, color: '#8892be' }]},
+        {name: 'Ottimizza Ltda.', title: 'SFTW ENGINEERING INTERN ', companyImg: 'images/ottimizza-icon.png', description: [
+                'At Ottimizza Automação Contábil in Brazil, I focused on automation projects using AWS Lambda and Python.',
+                'One project involved transforming bank statements into a compatible format for internal use across the company. Another extracted text from images in S3 buckets for integration into existing systems.',
+                'These tools improved efficiency and were deployed company-wide. I independently researched solutions and implemented cloud-based workflows.',
+                'This internship sharpened my skills in cloud computing, serverless architecture, and real-world problem solving.'
+            ]
+            , date: 'June 2023 - Aug 2023', stacks: [{icon: faJava, color: '#f89820'}, {icon: faDatabase, color: '#3776ab'}, {icon: faAws, color: '#ff9900'}]},
+        {name: 'Dendritic Hub', title: 'RESEARCH ASSISTANT', companyImg: 'images/fgcu-icon.png', description: [
+                'As a Research Assistant under Dr. De Castro at FGCU, I contribute to projects involving Evolutionary Computing.',
+                'I use Python, data structures, and algorithms to optimize everyday processes and improve system efficiency.',
+                'I also facilitate weekly research meetings to support collaboration and clarify research goals.',
+                'This role has strengthened my technical foundation and collaborative skills in an academic setting.'
+            ]
+            , date: 'Nov 2023 - Nov 2024', stacks: [{icon: faPython, color: '#3776ab'}]},
+        {name: 'U.A. Whitaker College of Engineering', title: 'SFTW ENGINEERING TUTOR', companyImg: 'images/fgcu-icon.png', description: [
+                'As a Software Engineering Tutor at FGCU, I provide academic support to students learning C, C++, and Python.',
+                'I simplify topics like pointers, loops, and object-oriented programming through one-on-one and group sessions.',
+                'Working with fellow tutors, I help develop effective teaching strategies that match different learning styles.',
+                'This experience has improved my communication, leadership, and technical mentoring skills.'
+            ]
+            , date: 'Aug 2023 - Nov 2024', stacks: [{icon: faPython, color: '#3776ab'}, {icon: faJava, color: '#f89820'}, {svg: 'images/cpp-icon.svg' , color: '#3776ab'}, {svg: 'images/c-icon.svg' , color: '#3776ab'}]},
+        {name: 'U.A. Whitaker College of Engineering', title: 'TEACHING ASSISTANT ', companyImg: 'images/fgcu-icon.png', description: [
+                'As a Teaching Assistant for Intro to Computer Science and Programming I & II, I support over 140 students at FGCU.',
+                'I assist with grading, provide detailed feedback on assignments, and help students grasp core programming concepts.',
+                'I also participate in course planning and assist in the classroom to reinforce instruction.',
+                'This role has enhanced my understanding of foundational programming and strengthened my ability to explain complex topics clearly.'
+            ]
+            , date: 'Aug 2023 - Nov 2024', stacks: [{icon: faPython, color: '#3776ab'}, {icon: faJava, color: '#f89820'}, {svg: 'images/cpp-icon.svg' , color: '#3776ab'}, {svg: 'images/c-icon.svg' , color: '#3776ab'}]},
 
     ];
 
     return (
-        <section id="experience" className="container_sec">
+        <section id="experience" className="container_sec mt-10">
             <h2 className='sectionHeader'>{sectionHeader}</h2>
-            <div className="flex flex-col gap-10 lg:flex-row transform-3d">
+            <div className="flex flex-col gap-10 lg:flex-row transform-3d" >
                 {companies.map(({name, title, companyImg, description, date, stacks}, index) => (
                     <div
                     onClick={() => setExpandedIndex(index)}
                     key={index}
-                    className={`w-full pt-6 pb-3 px-6  relative border border-[var(--clr-teal)] rounded-lg max-w-[500px] flex flex-col justify-between items-center bg-[var(--clr-darker)] transition-all duration-400 ease-in-out  ${expandedIndex === index ? 'min-h-[700px]' : 'min-h-60 cursor-pointer hover:translate-z-[200px] hover:brightness-100 hover:scale-[1.2] '} `}>
+                    className={`w-full pt-6 pb-3 px-6  relative border border-[var(--clr-teal)] rounded-lg max-w-[500px] flex flex-col justify-between items-center bg-[var(--clr-darker)] transition-all duration-400 ease-in-out  ${expandedIndex === index ? 'min-h-[700px]' : 'min-h-60 cursor-pointer hover:translate-z-[200px] hover:brightness-100 hover:scale-[1.1] hover:md:scale-[1.2] '} `}>
                         {/*Arrow up for link*/}
                                                                                                                                                                                                                                             {/*:hover{
                                                                                                                                                                                                                         transform: translateZ(200px);
