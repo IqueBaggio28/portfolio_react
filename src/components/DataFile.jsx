45ebf26ee7e0c35fa1a1f680cf9cc13ddf329b3e
IqueBaggio28 <iquebaggio@gmail.com>
2025-06-09 10:56:09 -0400

    docs: update about and goals text

--- src/components/DataFile.jsx
+++ src/components/DataFile.jsx
@@ -57,22 +57,25 @@ import SPshipLocation from "./images/projects/SP-ship-location.png";
 export const AboutData = [
   {
     title: "Introduction",
-    goals:
-      "Hi, I'm Henrique Baggio! I'm a Brazilian-born" +
-      " Software Engineering student with Italian citizenship, currently studying at Florida Gulf Coast University." +
-      " Even though I started coding a little later than others, I quickly caught up by diving into internships, " +
-      "research projects, and lots of self-learning. I work across the full stack — using React, Python, PHP, and" +
-      " C++ — and have experience building both academic and real-world applications. Whether it's front-end " +
-      "development, working with AWS, or managing SQL databases, I'm passionate about creating real solutions through code.",
+    about:
+      "Hi, I'm Henrique Baggio! I'm a Brazilian-born Software Engineer with Italian citizenship and a proud " +
+      "graduate of Florida Gulf Coast University. Since entering the field, I've been applying and expanding on " +
+      "the skills I developed through internships, research, and self-learning. I work across the full stack—using " +
+      "React, Python, PHP, and C++—and have experience building both academic and real-world applications. " +
+      "Whether it's front-end development, working with AWS, or managing SQL databases, I'm passionate about " +
+      "creating real solutions through code.",
   },
   {
     title: "Goals and Approach (FE / Full-Stack)",
     goals:
-      "I'm passionate about front-end and full-stack development, and I'm always working to sharpen my " +
-      "skills. I love building smooth, responsive web apps using React, JavaScript, and backend tools " +
-      "like PHP and SQL. My goal is to keep learning, work on real projects, contribute to open-source, " +
-      "and grow my network in the tech community. I'm excited to create user-friendly, high-impact " +
-      "applications while constantly improving and staying adaptable in this fast-moving industry.",
+      "I'm passionate about front-end and full-stack development, with a love for building smooth, " +
+      "responsive web apps using React, JavaScript, and backend tools like PHP and SQL. My primary goal " +
+      "for the future is to deepen my professional experience and continuously expand my knowledge in this " +
+      "fast-moving industry. I'm eager to tackle new challenges, contribute to impactful projects, and stay " +
+      "adaptable to grow as a software engineer and deliver high-quality, user-friendly applications.",
+  },
+  {
+    title: "Hobbies and Interests",
   },
 ];
 
